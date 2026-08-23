13abd303f266d71bd2cdfe62126b403cea8ff8e3
Yuran Saraiva <yrnsaraiva.office@gmail.com>
2026-08-23 19:37:32 +0200

    Add seed_demo.py

--- config/settings.py
+++ config/settings.py
@@ -55,10 +55,10 @@ TEMPLATES = [{
 #
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 POSTGRES_LOCALLY = True
